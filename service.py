99357e5efe513ab14c25c61f7d017a01994ee380
Ben Stobbs <ben@stobbs.com>
2021-06-02 11:04:14 +0100

    exponential movement

--- service.py
+++ service.py
@@ -1,8 +1,8 @@
-import time
 import Adafruit_ADS1x15
 import pyautogui
 
 MOVE_DISTANCE = 5
+SPEED_INCREASE = 1
 
 UP = 0
 DOWN = 2
@@ -13,6 +13,9 @@ THRESHOLD = 800
 
 adc = Adafruit_ADS1x15.ADS1015()
 
+x_increase = 0
+y_increase = 0
+
 while True:
     direction_values = [adc.read_adc(i, gain=1, data_rate=128) for i in [UP, DOWN, LEFT, RIGHT]]
 
@@ -22,16 +25,22 @@ while True:
     y_direction = 0
 
     if direction_values[0] > THRESHOLD:
-        y_direction = 1
+        y_increase += 1
+        y_direction = y_increase
     elif direction_values[1] > THRESHOLD:
-        y_direction = -1
+        y_increase -= 1
+        y_direction = -1 * y_increase
+    else:
+        y_increase = 0
 
     if direction_values[2] > THRESHOLD:
-        x_direction = -1
+        x_increase -= 1
+        x_direction = -1 * y_increase
     elif direction_values[3] > THRESHOLD:
-        x_direction = 1
+        x_increase += 1
+        x_direction = x_increase
+    else:
+        x_increase = 0
 
 
     pyautogui.drag(MOVE_DISTANCE * x_direction, MOVE_DISTANCE * y_direction, 0.05)
-
-    time.sleep(0.05)
