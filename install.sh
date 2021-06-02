4a36155a34ac8b241ff7e5365f1fcfc7c60d9bd6
Ben Stobbs <ben@stobbs.com>
2021-06-02 11:12:11 +0100

    made service

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [[ "$EUID" = 0 ]]; then
+    echo "Already root"
+else
+    sudo -k
+    if sudo true; then
+        echo "Sucessfully got root"
+    else
+        echo "Wrong password"
+        exit 1
+    fi
+fi
+
+sudo pip install Adafruit_ADS1x15
+
+sudo mkdir /etc/RPiSwitchJoystick
+sudo cp config.txt /etc/RPiSwitchJoystick/
+sudo cp service.py /etc/RPiSwitchJoystick/
+sudo cp RPiSwitchJoystick.service /lib/systemd/system/
+sudo chmod 644 /lib/systemd/system/RPiSwitchJoystick.service
+sudo chmod +x /etc/RPiSwitchJoystick/service.py
+
+sudo systemctl daemon-reload
+sudo systemctl enable RPiSwitchJoystick.service
+sudo systemctl start RPiSwitchJoystick.service
+
+echo "Done"
